3d1e5289094c853bd1e5b9282a8cfab23501804c
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-23 12:42:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    show = Show.maximum(:rating DESC).first
+    show = Show.maximum(:rating).first
     show.name
   end
 end
